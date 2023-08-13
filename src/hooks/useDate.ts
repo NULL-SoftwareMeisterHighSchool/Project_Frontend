a386e94c28264d39c051e76fd694286a0c783517
kwonkangbin <kbk282655@gmail.com>
2023-08-14 08:17:51 +0900

    DELETE : 1000px 제한 삭제

--- src/hooks/useDate.ts
+++ src/hooks/useDate.ts
@@ -1,6 +1,6 @@
 const useDate = (createdAt:string) => {
-    var date = createdAt.slice(0, 10).replaceAll('-','.');
-    var time = createdAt.slice(11, 16);
+    const date = createdAt.slice(0, 10).replaceAll('-','.');
+    const time = createdAt.slice(11, 16);
     return {date, time};
 }
    
