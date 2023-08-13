7601e445b980764a47177ea0cf56aba85bda335e
kimgyuha <kimgyuha25@gmail.com>
2023-08-13 15:06:15 +0900

    FIX : browserrouter errror

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import GlobalStyle from "@styles/global.style";
-import { BrowserRouter, RouterProvider } from "react-router-dom";
+import { RouterProvider } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "react-query";
 import { CookiesProvider } from "react-cookie";
 import { RecoilRoot } from "recoil";
