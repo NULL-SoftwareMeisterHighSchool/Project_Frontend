92ae8487da2f05c5a021652880f881360899768f
kwonkangbin <kbk282655@gmail.com>
2023-08-13 14:53:14 +0900

    CHORE : layouts 추가

---
+++ src/layouts/AppLayout/style.ts
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+    display: flex;
+`;
+export const Content = styled.div`
+    display: flex;
+    flex-direction: column;
+    gap:40px;
+    width: 100%;
+    padding: 60px 10%;
+    overflow-y: auto;
+    height: 100vh;
+`;
